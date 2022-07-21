23c6bc1664df671436ca09a0b8d7f5d37bc67e1f
Douglas Toledo <dwtoledo@outlook.com>
2022-07-21 10:56:32 -0300

    Add ObjToQueryString function and its Tests

---
+++ src/lib/quaryString.spec.ts
@@ -0,0 +1,32 @@
+const { objToQueryString } = require('./queryString');
+
+describe('Given an object input then the output should be the corresponding queryString', () => {
+
+    it('return the corresponding queryString', () => {
+        const input = {
+            name: "Douglas",
+            lastName: "Toledo",
+            inLoveWith: "Frontend"
+        }
+        expect(objToQueryString(input)).toBe('name=Douglas&lastName=Toledo&inLoveWith=Frontend');
+    });
+
+    it('return the corresponding queryString when Object values contain spaces', () => {
+        const input = {
+            name: "Douglas",
+            lastName: "Toledo",
+            inLoveWith: "Frontend Development"
+        }
+        expect(objToQueryString(input)).toBe('name=Douglas&lastName=Toledo&inLoveWith=Frontend%20Development');
+    });
+
+    it('return the corresponding queryString when Object values contain special characters', () => {
+        const input = {
+            name: "Douglas",
+            lastName: "Toledo",
+            socialNetworks: "@dwtoledo",
+            inLoveWith: "Front-end Development"
+        }
+        expect(objToQueryString(input)).toBe('name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development');
+    });
+});
---
+++ src/lib/queryString.ts
@@ -0,0 +1,5 @@
+module.exports.objToQueryString = (obj) => {
+    return Object.keys(obj).map((key) => {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
+    }).join('&');
+}
