1459215216adc9f84e15696a874a3451568f2c4d
Douglas Toledo <dwtoledo@outlook.com>
2022-07-21 11:30:13 -0300

    Refactor Func to Throw Error in case of Nested Obj

--- src/lib/queryString.spec.ts
+++ src/lib/queryString.spec.ts
@@ -8,7 +8,9 @@ describe('Given an object input then the output should be the corresponding quer
             lastName: "Toledo",
             inLoveWith: "Frontend"
         }
-        expect(objToQueryString(input)).toBe('name=Douglas&lastName=Toledo&inLoveWith=Frontend');
+        expect(objToQueryString(input)).toBe(
+            'name=Douglas&lastName=Toledo&inLoveWith=Frontend'
+        );
     });
 
     it('return the corresponding queryString when Object values contain spaces', () => {
@@ -17,7 +19,9 @@ describe('Given an object input then the output should be the corresponding quer
             lastName: "Toledo",
             inLoveWith: "Frontend Development"
         }
-        expect(objToQueryString(input)).toBe('name=Douglas&lastName=Toledo&inLoveWith=Frontend%20Development');
+        expect(objToQueryString(input)).toBe(
+            'name=Douglas&lastName=Toledo&inLoveWith=Frontend%20Development'
+        );
     });
 
     it('return the corresponding queryString when Object values contain special characters', () => {
@@ -27,7 +31,38 @@ describe('Given an object input then the output should be the corresponding quer
             socialNetworks: "@dwtoledo",
             inLoveWith: "Front-end Development"
         }
-        expect(objToQueryString(input)).toBe('name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development');
+        expect(objToQueryString(input)).toBe(
+            'name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development'
+        );
+    });
+
+    it('return the corresponding queryString when Object values contain array attributes', () => {
+        const input = {
+            name: "Douglas",
+            lastName: "Toledo",
+            socialNetworks: "@dwtoledo",
+            inLoveWith: "Front-end Development",
+            abilities: ['JS', 'TDD']
+        }
+        expect(objToQueryString(input)).toBe(
+            'name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development&abilities=JS%2CTDD'
+        );
+    });
+
+    it('throw an error when object input has one or more nested objects', () => {
+        const input = {
+            name: "Douglas",
+            lastName: "Toledo",
+            socialNetworks: "@dwtoledo",
+            inLoveWith: "Front-end Development",
+            abilities: {
+                frontend: ['HTML', 'CSS', 'JS'],
+                designer: ['UX, UI']
+            }
+        }
+        expect(() => {
+            objToQueryString(input)
+        }).toThrowError();
     });
 });
 
--- src/lib/queryString.ts
+++ src/lib/queryString.ts
@@ -1,5 +1,10 @@
 module.exports.objToQueryString = (obj) => {
     return Object.keys(obj).map((key) => {
+
+        if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+            throw new Error('Found nested objects into the input.')
+        }
+
         return `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
     }).join('&');
 }
