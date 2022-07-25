81f4797d84619667bcdc9b5aaeb64981ff5fde66
Douglas Toledo <dwtoledo@outlook.com>
2022-07-25 15:55:34 -0300

    Use Prettier to Format queryString Files

--- src/lib/queryString/queryString.spec.ts
+++ src/lib/queryString/queryString.spec.ts
@@ -1,115 +1,114 @@
-import { objToQueryString, queryStringToObj } from './queryString';
+import { objToQueryString, queryStringToObj } from "./queryString";
 
-describe('Given an object input then the output should be the corresponding queryString', () => {
+describe("Given an object input then the output should be the corresponding queryString", () => {
+  it("return the corresponding queryString", () => {
+    const input = {
+      name: "Douglas",
+      lastName: "Toledo",
+      inLoveWith: "Frontend",
+    };
+    expect(objToQueryString(input)).toBe(
+      "name=Douglas&lastName=Toledo&inLoveWith=Frontend"
+    );
+  });
 
-    it('return the corresponding queryString', () => {
-        const input = {
-            name: "Douglas",
-            lastName: "Toledo",
-            inLoveWith: "Frontend"
-        }
-        expect(objToQueryString(input)).toBe(
-            'name=Douglas&lastName=Toledo&inLoveWith=Frontend'
-        );
-    });
+  it("return the corresponding queryString when Object values contain spaces", () => {
+    const input = {
+      name: "Douglas",
+      lastName: "Toledo",
+      inLoveWith: "Frontend Development",
+    };
+    expect(objToQueryString(input)).toBe(
+      "name=Douglas&lastName=Toledo&inLoveWith=Frontend%20Development"
+    );
+  });
 
-    it('return the corresponding queryString when Object values contain spaces', () => {
-        const input = {
-            name: "Douglas",
-            lastName: "Toledo",
-            inLoveWith: "Frontend Development"
-        }
-        expect(objToQueryString(input)).toBe(
-            'name=Douglas&lastName=Toledo&inLoveWith=Frontend%20Development'
-        );
-    });
+  it("return the corresponding queryString when Object values contain special characters", () => {
+    const input = {
+      name: "Douglas",
+      lastName: "Toledo",
+      socialNetworks: "@dwtoledo",
+      inLoveWith: "Front-end Development",
+    };
+    expect(objToQueryString(input)).toBe(
+      "name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development"
+    );
+  });
 
-    it('return the corresponding queryString when Object values contain special characters', () => {
-        const input = {
-            name: "Douglas",
-            lastName: "Toledo",
-            socialNetworks: "@dwtoledo",
-            inLoveWith: "Front-end Development"
-        }
-        expect(objToQueryString(input)).toBe(
-            'name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development'
-        );
-    });
+  it("return the corresponding queryString when Object values contain array attributes", () => {
+    const input = {
+      name: "Douglas",
+      lastName: "Toledo",
+      socialNetworks: "@dwtoledo",
+      inLoveWith: "Front-end Development",
+      abilities: ["JS", "TDD"],
+    };
+    expect(objToQueryString(input)).toBe(
+      "name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development&abilities=JS%2CTDD"
+    );
+  });
 
-    it('return the corresponding queryString when Object values contain array attributes', () => {
-        const input = {
-            name: "Douglas",
-            lastName: "Toledo",
-            socialNetworks: "@dwtoledo",
-            inLoveWith: "Front-end Development",
-            abilities: ['JS', 'TDD']
-        }
-        expect(objToQueryString(input)).toBe(
-            'name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development&abilities=JS%2CTDD'
-        );
-    });
-
-    it('throw an error when object input has one or more nested objects', () => {
-        const input = {
-            name: "Douglas",
-            lastName: "Toledo",
-            socialNetworks: "@dwtoledo",
-            inLoveWith: "Front-end Development",
-            abilities: {
-                frontend: ['HTML', 'CSS', 'JS'],
-                designer: ['UX, UI']
-            }
-        }
-        expect(() => {
-            objToQueryString(input)
-        }).toThrowError();
-    });
-
+  it("throw an error when object input has one or more nested objects", () => {
+    const input = {
+      name: "Douglas",
+      lastName: "Toledo",
+      socialNetworks: "@dwtoledo",
+      inLoveWith: "Front-end Development",
+      abilities: {
+        frontend: ["HTML", "CSS", "JS"],
+        designer: ["UX, UI"],
+      },
+    };
+    expect(() => {
+      objToQueryString(input);
+    }).toThrowError();
+  });
 });
 
-describe('Given a queryString input then the output should be the corresponding object', () => {
+describe("Given a queryString input then the output should be the corresponding object", () => {
+  it("return the corresponding object", () => {
+    const input = "name=Douglas&lastName=Toledo&inLoveWith=Frontend";
+    const output = {
+      name: "Douglas",
+      lastName: "Toledo",
+      inLoveWith: "Frontend",
+    };
+    expect(queryStringToObj(input)).toEqual(output);
+  });
 
-    it('return the corresponding object', () => {
-        const input = 'name=Douglas&lastName=Toledo&inLoveWith=Frontend';
-        const output = {
-            name: "Douglas",
-            lastName: "Toledo",
-            inLoveWith: "Frontend"
-        };
-        expect(queryStringToObj(input)).toEqual(output);
-    });
+  it("return the corresponding object when Object values contain spaces", () => {
+    const input =
+      "name=Douglas&lastName=Toledo&inLoveWith=Frontend%20Development";
+    const output = {
+      name: "Douglas",
+      lastName: "Toledo",
+      inLoveWith: "Frontend Development",
+    };
+    expect(queryStringToObj(input)).toEqual(output);
+  });
 
-    it('return the corresponding object when Object values contain spaces', () => {
-        const input = 'name=Douglas&lastName=Toledo&inLoveWith=Frontend%20Development';
-        const output = {
-            name: "Douglas",
-            lastName: "Toledo",
-            inLoveWith: "Frontend Development"
-        };
-        expect(queryStringToObj(input)).toEqual(output);
-    });
+  it("return the corresponding object when queryString contain special characters", () => {
+    const input =
+      "name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development";
+    const output = {
+      name: "Douglas",
+      lastName: "Toledo",
+      socialNetworks: "@dwtoledo",
+      inLoveWith: "Front-end Development",
+    };
+    expect(queryStringToObj(input)).toEqual(output);
+  });
 
-    it('return the corresponding object when queryString contain special characters', () => {
-        const input = 'name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development';
-        const output = {
-            name: "Douglas",
-            lastName: "Toledo",
-            socialNetworks: "@dwtoledo",
-            inLoveWith: "Front-end Development"
-        }
-        expect(queryStringToObj(input)).toEqual(output);
-    });
-
-    it('return the corresponding object when queryString values contain array attributes', () => {
-        const input = 'name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development&abilities=JS%2CTDD';
-        const output = {
-            name: "Douglas",
-            lastName: "Toledo",
-            socialNetworks: "@dwtoledo",
-            inLoveWith: "Front-end Development",
-            abilities: ['JS', 'TDD']
-        }
-        expect(queryStringToObj(input)).toEqual(output);
-    });
-
+  it("return the corresponding object when queryString values contain array attributes", () => {
+    const input =
+      "name=Douglas&lastName=Toledo&socialNetworks=%40dwtoledo&inLoveWith=Front-end%20Development&abilities=JS%2CTDD";
+    const output = {
+      name: "Douglas",
+      lastName: "Toledo",
+      socialNetworks: "@dwtoledo",
+      inLoveWith: "Front-end Development",
+      abilities: ["JS", "TDD"],
+    };
+    expect(queryStringToObj(input)).toEqual(output);
+  });
 });
--- src/lib/queryString/queryString.ts
+++ src/lib/queryString/queryString.ts
@@ -1,27 +1,27 @@
 export function objToQueryString(obj) {
-    return Object.keys(obj).map((key) => {
+  return Object.keys(obj)
+    .map((key) => {
+      if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+        throw new Error("Found nested objects into the input.");
+      }
 
-        if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
-            throw new Error('Found nested objects into the input.')
-        }
-
-        return `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
-    }).join('&');
+      return `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
+    })
+    .join("&");
 }
 
 export function queryStringToObj(queryString) {
-    let object = {}
+  let object = {};
 
-    queryString.split('&').map(param => {
-        const keyValue = param.split('=');
+  queryString.split("&").map((param) => {
+    const keyValue = param.split("=");
 
-        if (decodeURIComponent(keyValue[1]).includes(',')) {
-            object[keyValue[0]] = decodeURIComponent(keyValue[1]).split(',');
+    if (decodeURIComponent(keyValue[1]).includes(",")) {
+      object[keyValue[0]] = decodeURIComponent(keyValue[1]).split(",");
+    } else {
+      object[keyValue[0]] = decodeURIComponent(keyValue[1]);
+    }
+  });
 
-        } else {
-            object[keyValue[0]] = decodeURIComponent(keyValue[1]);
-        }
-    });
-
-    return object;
+  return object;
 }
