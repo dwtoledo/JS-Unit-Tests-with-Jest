f86342093bd15cba68af6111af36edb9336e4cbd
Douglas Toledo <dwtoledo@outlook.com>
2022-08-19 16:21:36 -0300

    Add Unit Test for getTotalAfterDiscount function on PercentageCondition Class

--- src/lib/shopping-cart/discounts/PercentageCondition.spec.ts
+++ src/lib/shopping-cart/discounts/PercentageCondition.spec.ts
@@ -1,6 +1,8 @@
+import Item from "../Item";
+import Product from "../Product";
 import { PercentageCondition } from "./PercentageCondition";
 
-describe("Percentage Discount Condition Class", () => {
+describe("Percentage Condition Class", () => {
   it("should throw an error when creates a percentage discount condition with quantity less than 2", () => {
     expect(() => {
       const test = new PercentageCondition(90, 1);
@@ -23,5 +25,17 @@ describe("Percentage Discount Condition Class", () => {
     expect(() => {
       const test = new PercentageCondition(-30, 2);
     }).toThrowError();
+  });
+
+  it('should return the correct total after discount', () => {
+    let mocked_product_1: Product;
+    mocked_product_1 = new Product();
+    mocked_product_1.setName("Mocked Product 1");
+    mocked_product_1.setPrice(10);
+
+    const mocked_item_1 = new Item(2, mocked_product_1);
+    const percentageCondition = new PercentageCondition(15, 2);
+
+    expect(percentageCondition.getTotalAfterDiscount(mocked_item_1)).toBe(17);
   });
 });
